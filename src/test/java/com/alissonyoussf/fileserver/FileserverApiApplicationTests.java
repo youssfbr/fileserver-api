96c99acccfb578f22d66c200ce9163de39fe6b64
Alisson Youssf <youssfbr@gmail.com>
2019-10-05 08:04:55 -0300

    Projeto criado

---
+++ src/test/java/com/alissonyoussf/fileserver/FileserverApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alissonyoussf.fileserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileserverApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
