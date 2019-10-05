bae6c026b681a256cb333155d1a36861b322600a
Alisson Youssf <youssfbr@gmail.com>
2019-10-05 08:19:09 -0300

    Criada variaveis dentro do Component Disco

--- src/main/java/com/alissonyoussf/fileserver/storage/Disco.java
+++ src/main/java/com/alissonyoussf/fileserver/storage/Disco.java
@@ -1,5 +1,14 @@
 package com.alissonyoussf.fileserver.storage;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Disco {
 
+	@Value("${arquivo.disco.raiz}")
+	private String raiz;
+	
+	@Value("${arquivo.disco.diretorio-arquivos}")
+	private String diretorioArquivos;
 }
