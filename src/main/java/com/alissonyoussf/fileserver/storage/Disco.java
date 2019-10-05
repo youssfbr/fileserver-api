487801cb0f2bfc9e606c5a0c910168c43c9665a6
Alisson Youssf <youssfbr@gmail.com>
2019-10-05 08:43:20 -0300

    Criado metodo salvar()

--- src/main/java/com/alissonyoussf/fileserver/storage/Disco.java
+++ src/main/java/com/alissonyoussf/fileserver/storage/Disco.java
@@ -1,7 +1,13 @@
 package com.alissonyoussf.fileserver.storage;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class Disco {
@@ -11,4 +17,16 @@ public class Disco {
 	
 	@Value("${arquivo.disco.diretorio-arquivos}")
 	private String diretorioArquivos;
+	
+	public void salvar(String diretorio, MultipartFile arquivoUpload) {
+		Path diretorioPath = Paths.get(this.raiz, diretorio);
+		Path arquivoPath = diretorioPath.resolve(arquivoUpload.getOriginalFilename());
+		
+		try {
+			Files.createDirectories(diretorioPath);
+			arquivoUpload.transferTo(arquivoPath.toFile());
+		} catch (IOException e) {
+			throw new RuntimeException("Não foi possível salvar o arquivo");
+		}
+	}
 }
