88ba8ab8ab015710460280af2bc9ee3a4d5d58ae
Alisson Youssf <youssfbr@gmail.com>
2019-10-06 07:48:42 -0300

    API terminada

--- src/main/java/com/alissonyoussf/fileserver/controller/ArquivosController.java
+++ src/main/java/com/alissonyoussf/fileserver/controller/ArquivosController.java
@@ -1,5 +1,25 @@
 package com.alissonyoussf.fileserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.alissonyoussf.fileserver.storage.Disco;
+
+@RestController
+@RequestMapping("/arquivos")
+@CrossOrigin("*")
 public class ArquivosController {
 
+	@Autowired
+	private Disco disco;
+		
+	@PostMapping
+	public void upload(@RequestParam MultipartFile arquivo) {
+		disco.salvarArquivo(arquivo);		
+	}
 }
